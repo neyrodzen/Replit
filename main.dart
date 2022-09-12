752fd5d1690aecc89c1a43effbe75696b485a745
Aleksandr Kononov <108976436+neyrodzen@users.noreply.github.com>
2022-09-12 09:43:24 +0000

    Factory Method

--- main.dart
+++ main.dart
@@ -1,6 +1,17 @@
-void main() {
-Parent obj = Child();
- obj.hello;
+void main(){
+final fact = ChoiceFactory('2');
+  final obj = fact?.createChild();
+ print(obj.runtimeType); 
+}
+
+factoryMethod? ChoiceFactory(String str){
+factoryMethod?  Factory;
+  if(str == "1")
+Factory=  factoryChild_1();
+else if(str == "2")
+Factory=  factoryChild_2();
+  else Factory= null;
+  return Factory;
 }
 
 class Parent {
@@ -11,14 +22,14 @@ print('hello Parent');
 }
 
 class Child implements Parent{
-int b=3;
+// int b=3;
  // A(this.b){}
  void hello(){
 print('hello Child');  
   }
 }
 class Child_2 implements Parent{
-int b=3;
+//int b=3;
  // A(this.b){}
  void hello(){
 print('hello Child_2');  
@@ -26,5 +37,17 @@ print('hello Child_2');
 }
 
 
+abstract class factoryMethod{
+  Parent createChild();
+}
 
- 
+class factoryChild_1 implements factoryMethod{
+  Parent createChild(){
+  return Child();
+  }
+}
+ class factoryChild_2 implements factoryMethod{
+   Parent createChild(){
+   return Child_2();
+  }
+ }
